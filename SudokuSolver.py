56c58318d4af47f4202d2d2b2c6a184b865c9eac
lausan3 <lauanthony4455@gmail.com>
2023-05-09 11:17:21 -0400

    Added basics

---
+++ SudokuSolver.py
@@ -0,0 +1,9 @@
+class SudokuSolver:
+    BOARD_SIZE = 9
+    board = [BOARD_SIZE][BOARD_SIZE]
+    
+    def __init__():
+        for row in range(BOARD_SIZE):
+            for col in range(BOARD_SIZE):
+
+        solvable = False
